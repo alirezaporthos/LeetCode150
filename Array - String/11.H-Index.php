f6b61cfe89f32bd45e56bda1345c2e54925050e5
alirezaporthos <alirezaportous@gmail.com>
2024-07-26 13:28:05 +0330

    Add Array-String/ H-Index

---
+++ Array - String/11.H-Index.php
@@ -0,0 +1,33 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $citations
+     * @return Integer
+     */
+    function hIndex($citations)
+    {
+        // first answer
+        $articlesCount = count($citations);
+        $hIndex = $articlesCount;
+        $errorChances = 0;
+        $success = false;
+
+        while (!$success) {
+            $errorChances = $articlesCount - $hIndex;
+            for ($i = 0; $i < $articlesCount; $i++) {
+                if ($citations[$i] < $hIndex) {
+                    $errorChances--;
+                }
+                if ($errorChances < 0) {
+                    $hIndex--;
+                    break;
+                }
+                if ($errorChances + 1 >= $articlesCount - $i)
+                    $success = true;
+            }
+        }
+    }
+}
