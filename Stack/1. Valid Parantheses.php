3eac0e913d5b7a349d0074480a01b27efccd3eb8
alirezaporthos <alirezaportous@gmail.com>
2024-07-27 22:15:47 +0330

    Add second Stack/IsValid

--- Stack/1. Valid Parantheses.php
+++ Stack/1. Valid Parantheses.php
@@ -7,34 +7,54 @@ class Solution
      * @param String $s
      * @return Boolean
      */
+
+    //o(n) *
     function isValid($s)
     {
         $stack = [];
         $parantheses = [
-            ')' => '(',
-            '}' => '{',
-            ']' => '['
+            '(' => ')',
+            '[' => ']',
+            '{' => '}'
         ];
 
-        // O(n)
-        for ($i = 0; $i < strlen($s); $i++) {
-            $char = $s[$i];
-
-            if (
-                $char === '(' or
-                $char === '{' or
-                $char === '['
-            ) {
-                $stack[] = $char;
+        for ($i = 0, $length = strlen($s); $i < $length; $i++) {
+            if (array_key_exists($s[$i], $parantheses)) {
+                $stack[] = $s[$i];
             } else {
-                if (end($stack) === $parantheses[$char]) {
-                    array_pop($stack);
-                } else {
+                if (empty($stack) or $parantheses[end($stack)] !== $s[$i]) {
                     return false;
                 }
+                array_pop($stack);
             }
         }
-
         return empty($stack);
+
+        // $parantheses = [
+        //     ')' => '(',
+        //     '}' => '{',
+        //     ']' => '['
+        // ];
+
+        // // O(n)
+        // for ($i = 0; $i < strlen($s); $i++) {
+        //     $char = $s[$i];
+
+        //     if (
+        //         $char === '(' or
+        //         $char === '{' or
+        //         $char === '['
+        //     ) {
+        //         $stack[] = $char;
+        //     } else {
+        //         if (end($stack) === $parantheses[$char]) {
+        //             array_pop($stack);
+        //         } else {
+        //             return false;
+        //         }
+        //     }
+        // }
+
+        // return empty($stack);
     }
 }
